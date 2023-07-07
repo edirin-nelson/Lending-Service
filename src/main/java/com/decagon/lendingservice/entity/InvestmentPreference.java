6ff816a4afd959b76340d9377d96740f8f71013e
edirin nelson <nelsonedirin@gmail.com>
2023-07-07 16:42:37 +0100

    created entity for loan and investmentPreference

---
+++ src/main/java/com/decagon/lendingservice/entity/InvestmentPreference.java
@@ -0,0 +1,10 @@
+package com.decagon.lendingservice.entity;
+
+import java.math.BigDecimal;
+
+public class InvestmentPreferences {
+    private 
+    private BigDecimal loanAmount;
+    private BigDecimal interestRate;
+    private int riskTolerance;
+}
---
+++ src/main/java/com/decagon/lendingservice/entity/Loan.java
@@ -0,0 +1,2 @@
+package com.decagon.lendingservice.entity;public class Loan {
+}
